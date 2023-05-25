537b986ee8fbd87bbbd772078c292d43c423efc0
mkhululi <mkhululimtshali@gmail.com>
2023-05-25 12:33:10 +0200

    added functionality for join, join queue and depart buttons

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,11 +1,27 @@
-// write your DOM code here.
-
-
 // DOM element references
+const joinBtn = document.querySelector(".join_queue");
+const joinQueueBtn = document.querySelector(".join_taxi_queue");
+const departBtn = document.querySelector(".depart");
+const passengerCountEl = document.querySelector(".passenger_queue_count");
+const taxiCountEl = document.querySelector(".taxi_queue_count");
 
 // create Factory Function instance
-
 const taxiQueue = TaxiQueue();
-
+// Functions
+function joinButtonFunction() {
+  taxiQueue.joinQueue();
+  passengerCountEl.innerHTML = taxiQueue.queueLength();
+}
+function joinQueueButtonFunction() {
+  taxiQueue.joinTaxiQueue();
+  taxiCountEl.innerHTML = taxiQueue.taxiQueueLength();
+}
+function departButtonFunction() {
+  taxiQueue.taxiDepart();
+  passengerCountEl.innerHTML = taxiQueue.queueLength();
+  taxiCountEl.innerHTML = taxiQueue.taxiQueueLength();
+}
 // DOM events
-
+joinBtn.addEventListener("click", joinButtonFunction);
+joinQueueBtn.addEventListener("click", joinQueueButtonFunction);
+departBtn.addEventListener("click", departButtonFunction);
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,36 +1,43 @@
 function TaxiQueue() {
+  let passengersCount = 0;
+  let numberOfTaxis = 0;
+  function joinQueue() {
+    passengersCount++;
+  }
 
+  function leaveQueue() {}
 
-	function joinQueue() {
+  function joinTaxiQueue() {
+    numberOfTaxis++;
+  }
 
-	}
+  function queueLength() {
+    return passengersCount;
+  }
 
-	function leaveQueue() {
+  function taxiQueueLength() {
+    return numberOfTaxis;
+  }
 
-	}
+  function taxiDepart() {
+    // console.log();
+    // check if the number of taxis in the queue is greater than zero and number of
+    // passangers is greater than 11.
+    // then.
+    // minus one taxi from the queue each time
+    // minus 12 passengers each time.
+    if (queueLength() > 11 && taxiQueueLength()) {
+      passengersCount = queueLength() - 12;
+      numberOfTaxis = taxiQueueLength() - 1;
+    }
+  }
 
-	function joinTaxiQueue() {
-
-	}
-
-	function queueLength() {
-
-	}
-
-	function taxiQueueLength() {
-
-	}
-
-	function taxiDepart(){
-
-	}
-
-	return {
-		joinQueue,
-		leaveQueue,
-		joinTaxiQueue,
-		queueLength,
-		taxiQueueLength,
-		taxiDepart
-	}
+  return {
+    joinQueue,
+    leaveQueue,
+    joinTaxiQueue,
+    queueLength,
+    taxiQueueLength,
+    taxiDepart,
+  };
 }
